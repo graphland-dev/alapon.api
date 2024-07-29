c213a41ebd2158f9784eb0a22dce7aaf5a681b0e
kingrayhan <rayhan095@gmail.com>
2024-07-29 22:09:08 +0600

    room list

--- client/src/pages/chat/components/ChatRoomItem.tsx
+++ client/src/pages/chat/components/ChatRoomItem.tsx
@@ -1,22 +1,27 @@
+import { ChatRoom } from '@/common/api-models/graphql';
 import { Link } from 'react-router-dom';
 
-const ChatRoomItem = () => {
+interface Props {
+  room: ChatRoom;
+}
+const ChatRoomItem: React.FC<Props> = ({ room }) => {
   return (
     <Link
       className="flex gap-2 p-3 border-b cursor-pointer hover:bg-zinc-100"
-      to={'/chat/1'}
+      to={`/chat/${room?._id}`}
+      data-room-id={room?._id}
     >
-      {/* <div>
+      <div>
         <img
           className="w-12 h-12 rounded-full "
-          src="https://pps.whatsapp.net/v/t61.24694-24/200301938_505519510885427_5383825801587828370_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdSvWJpAwvhxVlZvLEt1jYsiGJVaYaWcD2ezCkXeRE1UlQ&oe=635E44AE"
-          alt=""
+          src="https://avatars.githubusercontent.com/u/7611746?v=4"
+          alt={room?.handle || ''}
         />
-      </div> */}
+      </div>
       <div className="flex flex-col items-start gap-1">
-        <p>@luc_zonedddd</p>
+        <p>@{room.handle}</p>
         <p className="px-1 text-xs rounded-sm bg-primary text-primary-foreground">
-          group
+          {room.roomType}
         </p>
         {/* <p className="text-sm text-zinc-500">Vai kemon achen???</p> */}
       </div>
--- client/src/pages/chat/components/ChatSidebar.tsx
+++ client/src/pages/chat/components/ChatSidebar.tsx
@@ -1,12 +1,31 @@
+import { gql, useQuery } from '@apollo/client';
 import ChatRoomItem from './ChatRoomItem';
+import { ChatRoomsWithPagination } from '@/common/api-models/graphql';
+
+const MY_CHAT_ROOMS_QUERY = gql`
+  query Chat__myChatRooms($where: CommonPaginationOnlyDto) {
+    chat__myChatRooms(where: $where) {
+      nodes {
+        _id
+        handle
+        isNsfw
+        roomType
+      }
+    }
+  }
+`;
 
 const ChatSidebar = () => {
+  const { data, loading } = useQuery<{
+    chat__myChatRooms: ChatRoomsWithPagination;
+  }>(MY_CHAT_ROOMS_QUERY, { variables: { where: { limit: -1 } } });
+
   return (
     <div className="flex flex-col h-full">
       <div className="h-[65px] flex-none">@user</div>
       <div className="flex-auto overflow-y-auto">
-        {Array.from({ length: 100 }).map((_, i) => (
-          <ChatRoomItem key={i} />
+        {data?.chat__myChatRooms.nodes?.map((room) => (
+          <ChatRoomItem key={room._id} room={room} />
         ))}
       </div>
     </div>
