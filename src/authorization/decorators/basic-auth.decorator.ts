6213f4f4471b61b28dc6275357a10d25cc8b2033
kingrayhan <rayhan095@gmail.com>
2024-07-29 12:35:15 +0600

    remove swagger

--- src/authorization/decorators/basic-auth.decorator.ts
+++ src/authorization/decorators/basic-auth.decorator.ts
@@ -1,7 +1,8 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBasicAuth } from '@nestjs/swagger';
-
 export const BasicAuth = () => {
-  return applyDecorators(UseGuards(AuthGuard('basic-auth')), ApiBasicAuth());
+  return applyDecorators(
+    UseGuards(AuthGuard('basic-auth')),
+    // ApiBasicAuth()
+  );
 };
