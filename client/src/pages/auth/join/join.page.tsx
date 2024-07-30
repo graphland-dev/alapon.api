e7349971266fea4724cf45e28537cd344bf4fadc
kingrayhan <rayhan095@gmail.com>
2024-07-30 10:57:46 +0600

    client: lottie animation for empty room list

--- client/src/pages/auth/join/join.page.tsx
+++ client/src/pages/auth/join/join.page.tsx
@@ -66,7 +66,7 @@ const JoinPage = () => {
               </Button>
 
               <Link to={'/auth/login'} className="link">
-                Logi now
+                Login now
               </Link>
             </div>
           </div>
--- client/src/pages/chat/components/ChatSidebar.tsx
+++ client/src/pages/chat/components/ChatSidebar.tsx
@@ -1,21 +1,22 @@
 import { ChatRoomsWithPagination } from '@/common/api-models/graphql';
 import { userAtom } from '@/common/states/user.atom';
+import { TokenService } from '@/common/utils/TokenService';
 import { gql, useQuery } from '@apollo/client';
-import { Menu, Modal, Skeleton, UnstyledButton } from '@mantine/core';
+import { Menu, Modal, Skeleton, UnstyledButton, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { openConfirmModal } from '@mantine/modals';
+import EmptyChatLottieFile from '@/common/lottie-files/empty-chat-lottie.json';
 import {
   IconChevronDown,
   IconDotsVertical,
   IconLogout,
-  IconPlus,
 } from '@tabler/icons-react';
 import { useAtomValue } from 'jotai';
+import Lottie from 'lottie-react';
 import ChatRoomItem from './ChatRoomItem';
-import { openConfirmModal } from '@mantine/modals';
-import { TokenService } from '@/common/utils/TokenService';
-import { useDisclosure } from '@mantine/hooks';
 import CreateGroupForm from './modal-forms/CreateGroupForm';
-import JoinInPersonForm from './modal-forms/JoinInPersonForm';
 import JoinInGroupForm from './modal-forms/JoinInGroupForm';
+import JoinInPersonForm from './modal-forms/JoinInPersonForm';
 
 const MY_CHAT_ROOMS_QUERY = gql`
   query Chat__myChatRooms($where: CommonPaginationOnlyDto) {
@@ -103,6 +104,42 @@ const ChatSidebar = () => {
               {Array.from({ length: 10 }).map((_, i) => (
                 <Skeleton key={i} height={65} width="100%" />
               ))}
+            </div>
+          )}
+
+          {!loading && data?.chat__myChatRooms?.nodes?.length === 0 && (
+            <div className="grid w-10/12 mx-auto mt-2">
+              <div className="text-center">
+                <p className="text-lg font-bold text-slate-500">
+                  You have no chat yet. Create one now!
+                </p>
+                <div className="w-8/12 mx-auto">
+                  <Lottie animationData={EmptyChatLottieFile} />
+                </div>
+                <p className="text-sm text-slate-500">
+                  Connect with friends/Groups, or create your own group.
+                </p>
+
+                <Menu>
+                  <Menu.Target>
+                    <UnstyledButton className="flex items-center gap-1 mx-auto mt-2 link">
+                      <p>Actions</p>
+                      <IconChevronDown size={14} />
+                    </UnstyledButton>
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <Menu.Item onClick={joinInPersonModalHandler.open}>
+                      Join in person
+                    </Menu.Item>
+                    <Menu.Item onClick={createGroupModalHandler.open}>
+                      Create Group
+                    </Menu.Item>
+                    <Menu.Item onClick={joinInGroupModalHandler.open}>
+                      Join Group
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </div>
             </div>
           )}
 
