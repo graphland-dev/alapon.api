ef380ae314c24b815c3a7ff84a4d01f1730d7d40
kingrayhan <rayhan095@gmail.com>
2024-07-30 21:41:46 +0600

    client:pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+// const manifestForPlugin: Partial<VitePWAOptions> = {
+//   registerType: 'prompt',
+//   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+//   manifest: {
+//     name: 'Weather Ups',
+//     short_name: 'Weathe Ups',
+//     description: 'An app that can show weather forecast for your city.',
+//     theme_color: '#171717',
+//     background_color: '#e8ebf2',
+//     display: 'standalone',
+//     scope: '/',
+//     start_url: '/',
+//     orientation: 'portrait',
+//   },
+// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       '/socket': {
         target: 'http://localhost:4001',
